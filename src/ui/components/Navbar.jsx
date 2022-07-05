e50ffaab1b3fab9882c97e1fa93cf6bcf1dbce79
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-07-04 23:35:53 -0300

    Se procesan rutas

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+    const navigate = useNavigate();
+    const onLogout = () => {
+        navigate('/login', { replace: true });
+    };
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             <Link
@@ -29,6 +33,14 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+                    <NavLink
+                        className={({ isActive }) =>
+                            `nav-item nav-link ${isActive ? 'active' : ''}`
+                        }
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
@@ -37,7 +49,12 @@ export const Navbar = () => {
                     <span className="nav-item nav-link text-primary">
                         Mauricio
                     </span>
-                    <button className="nav-item nav-link btn">Logout</button>
+                    <button
+                        className="nav-item nav-link btn"
+                        onClick={onLogout}
+                    >
+                        Logout
+                    </button>
                 </ul>
             </div>
         </nav>
